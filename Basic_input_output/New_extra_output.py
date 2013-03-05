b3685ab4134a05f0e412cd4b754d4284695079c9
Lori J <mintblue87@gmail.com>
2013-03-04 18:40:38 -0800

    trying to resolve extra_output

--- Basic_input_output/New_extra_output.py
+++ Basic_input_output/New_extra_output.py
@@ -12,12 +12,9 @@ directory =  '/Users/nebula/Python/Basic_input_output'
 
 print PmagPy_tests.universal_variable
 
-# Test_instance
 # def __init__(self, name, infile, outfile, ref_out, wrong_out, stdin, WD, *args):
 
-#new 
 
-# tag, as -Fsa, or whatever
 
 
 # iterates through a list of tuples with files, and compares them.  Order is: (output, correct_reference, incorrect_reference)
@@ -47,23 +44,31 @@ def iterate_through(some_list):
         print str(z) + " iterations"
 
 class Ex_out(Rename_me.Test_instance):
-
-    def __init__(self, name, infile):
+# can add in some, if file != None, then add the file_prefix
+    def __init__(self, name, infile, tag1, outfile1, tag2, outfile2, correctfile1, correctfile2, wrongfile1, wrongfile2, stdin=None, WD=False):
         self.name = name
-        self.infile = infile
+        self.infile = file_prefix + infile
+        self.tag1 = tag1
+        self.outfile1 = file_prefix + outfile1
+        self.tag2 = tag2
+        self.outfile2 = file_prefix + outfile2
+        self.correctfile1 = file_prefix + correctfile1
+        self.correctfile2 = file_prefix + correctfile2
+        self.wrongfile1 = file_prefix + wrongfile1
+        self.wrongfile2 = file_prefix + wrongfile2
+        self.stdin = stdin
+        self.WD = WD
+        if self.WD:
+            self.infile, self.outfile1, self.outfile2, self.correctfile1, self.correctfile2, self.wrongfile1, self.wrongfile2 = infile, outfile1, outfile2, correctfile1, correctfile2, wrongfile1, wrongfile2
 
-    def add_attr(self, Fa, Fr, Fa_reference = None, Fr_reference = None, Fa_wrong = None, Fr_wrong = None):
-#        self.input_file = input_file
-        self.Fa = Fa
-        self.Fr = Fr
-        self.Fa_reference = Fa_reference
-        self.Fr_reference = Fr_reference
-        self.Fa_wrong = Fa_wrong
-        self.Fr_wrong = Fr_wrong
-
-
-    def run_with_two_options(self):
-        env.run(self.name, '-WD', directory, '-f', self.input_file , '-Fa', self.Fa, '-Fr', self.Fr)
+    def run_program(self):
+        if self.WD:
+            obj = env.run(self.name, '-WD', directory, '-f', self.infile, self.tag1, self.outfile1, self.tag2, self.outfile2, stdin=self.stdin)
+        else:
+            obj = env.run(self.name, '-f', self.infile, self.tag1, self.outfile1, self.tag2, self.outfile2, stdin=self.stdin)
+        print "stdout: " +str(obj.stdout)
+        print "created: " + str(obj.files_created)
+        print "updated: " + str(obj.files_updated)
 
     def check_attr(self):
         Fa = self.parse_file(self.Fa)
@@ -83,34 +88,33 @@ class Ex_out(Rename_me.Test_instance):
             print str(self.name) + " raised error"
             raise NameError(self.Fr + " was incorrect")
 
-        
-# aarm_magic test
-aarm_magic_in_file = "aarm_measurements.txt"
-#fsa = 'er_samples.txt' appears not to matter.....
-aarm_Fa, aarm_Fr = 'aarm_magic_anisotropy.out', 'aarm_magic_results.out'
-aarm_Fa_reference, aarm_Fr_reference = 'aarm_magic_anisotropy_correct.out', 'aarm_magic_results_correct.out'
-aarm_Fa_wrong, aarm_Fr_wrong = 'aarm_magic_anisotropy_incorrect.out', 'aarm_magic_results_incorrect.out'
-aarm_magic = Ex_out('aarm_magic.py', aarm_magic_in_file)
 
-def complete_aarm_magic_test():   
-    print "-"
-    print "Testing aarm_magic.py:"
-    aarm_magic.add_attr(aarm_magic_in_file, aarm_Fa, aarm_Fr, aarm_Fa_reference, aarm_Fr_reference, aarm_Fa_wrong, aarm_Fr_wrong)
-    aarm_magic.run_with_two_options()
-    aarm_magic.check_attr()
+#    def __init__(self, name, infile, tag1, outfile1, tag2, outfile2, correctfile1, correctfile2, wrongfile1, wrongfile2, stdin=None, WD=False):
+
+
+def aarm_magic_test():
+    print "Testing aarm_magic"
+    infile = "aarm_measurements.txt"
+#fsa = 'er_samples.txt' appears not to matter.....
+    aarm_Fa, aarm_Fr = 'aarm_magic_anisotropy.out', 'aarm_magic_results.out'
+    tag1, tag2 = "-Fa", "-Fr"
+    aarm_Fa_reference, aarm_Fr_reference = 'aarm_magic_anisotropy_correct.out', 'aarm_magic_results_correct.out'
+    aarm_Fa_wrong, aarm_Fr_wrong = 'aarm_magic_anisotropy_incorrect.out', 'aarm_magic_results_incorrect.out'
+    aarm_magic = Ex_out('aarm_magic.py', infile, tag1, aarm_Fa, tag2, aarm_Fr, aarm_Fa_reference, aarm_Fr_reference, aarm_Fa_wrong, aarm_Fr_wrong)
+    aarm_magic.run_program()
+
+aarm_magic_test()
+
 
 class Bad_aarm_magic(unittest.TestCase):
-    def test_for_error(self):
-        aarm_magic.add_attr(aarm_magic_in_file, aarm_Fa, aarm_Fr, aarm_Fa_wrong, aarm_Fr_wrong, aarm_Fa_reference, aarm_Fr_reference)
-        aarm_magic.run_with_two_options()
-        self.assertRaises(NameError, aarm_magic.check_attr)         
+    pass
 
 # atrm_magic.py test
 atrm_in_file = 'atrm_magic_example.dat'
 atrm_Fa, atrm_Fr = "atrm_anisotropy.txt", "atrm_results.txt"
 atrm_Fa_reference, atrm_Fr_reference = "atrm_anisotropy_correct.txt", "atrm_results_correct.txt"
 atrm_Fa_wrong, atrm_Fr_wrong = "atrm_anisotropy_incorrect.txt", "atrm_results_incorrect.txt"
-atrm_magic = Ex_out('atrm_magic.py', atrm_in_file)
+#atrm_magic = Ex_out('atrm_magic.py', atrm_in_file)
 
 def complete_atrm_magic_test():
     print "-"
@@ -120,11 +124,13 @@ def complete_atrm_magic_test():
     atrm_magic.check_attr()
 
 class Bad_atrm(unittest.TestCase):
+    pass
+no = """
     def test_for_error(self):
         atrm_magic.add_attr(atrm_in_file, atrm_Fa, atrm_Fr, atrm_Fa_wrong, atrm_Fr_wrong, atrm_Fa_reference, atrm_Fr_reference)
         atrm_magic.run_with_two_options()
         self.assertRaises(NameError, atrm_magic.check_attr)
-
+"""
 hysteresis_magic_infile = 'hysteresis_magic_example.dat'
 hysteresis_F, hysteresis_F_reference, hysteresis_F_wrong = file_prefix + 'hysteresis_magic_rmag.txt', file_prefix + 'hysteresis_magic_rmag_correct.txt', file_prefix + 'hysteresis_magic_rmag_incorrect.txt'
 hysteresis_remanence, hysteresis_remanence_reference, hysteresis_remanence_wrong = file_prefix + 'rmag_remanence.txt', file_prefix + 'hysteresis_magic_remanence_correct.txt', file_prefix + 'hysteresis_magic_remanence_incorrect.txt'
@@ -145,7 +151,7 @@ def complete_hysteresis_magic_test():
     subprocess.call(['mv', file_prefix + 'rmag_remanence.txt', file_prefix + 'hysteresis_magic_remanence.txt']) # renames file so it is easier to find
 
 
-complete_hysteresis_magic_test()
+#complete_hysteresis_magic_test()
 
 #k15_magic.py test
 k15_in_file = 'k15_magic_example.dat'
@@ -227,6 +233,6 @@ def complete_working_test():
 
 
 if __name__ == "__main__":
-#    pass
-    complete_working_test()
+    pass
+#    complete_working_test()
 # fails to do this because it quits after unittest : (
